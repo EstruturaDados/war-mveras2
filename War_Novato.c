9548b22ec4c106835ab75f831c146c5d982c4834
mveras2 <202501504353@alunos.estacio.br>
2026-04-27 12:43:51 +0000

    27-04-2026

--- War_Novato.c
+++ War_Novato.c
@@ -16,7 +16,7 @@
     - Impressão organizada dos dados de todos os territórios
 */
 // ============================================================================
-/*
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,10 +27,10 @@
 
 // --- Definição da Estrutura (struct)
 struct Territorio {
-        int NUM_TERRITORIO;
+        int TERRITORIO;
         char NOME[TAM_STRING];
         char COR[TAM_STRING];
-        char TROPAS[TAM_STRING];
+        int TROPAS;
         char MAPA[TAM_STRING];
 };
 
@@ -39,17 +39,100 @@ void limpaBufferEntrada() {
         int c;
         while ((c = getchar()) != '\n' && c != EOF);
 }
-*/
+
 int main(){
-/*        struct Territorio biblioteca[NUM_TERRITORIO];
+        struct Territorio biblioteca[NUM_TERRITORIO];
         int totalterritorio = 0;
         int opcao;
+// Programa Principal
+// Cadastra os detalhes de cada Exército
 
-totalterritorio = 1;
-printf("territorio = %d\n", totalterritorio);
-*/
+//////////////////////////////////////////////////
+        do{
 
-printf("ola mundo!");
+            //Exibe o menu de opções.
+            printf("=============================\n");
+            printf("    LISTA DE EXERCITO        \n");
+            printf("=============================\n");
+            printf("1 - CADASTRAR NOVO EXERCITO  \n");
+            printf("2 - LISTAR TODOS OS EXERCITOS\n");
+            printf("0 - SAIR                     \n");
 
+            // Lê a opção do usuário.
+            scanf("%d", &opcao);
+            limpaBufferEntrada(); //Limpa algum comando inválido deixado pelo scanf.
+
+            // --- Processamento da Opção ---
+
+            switch (opcao) {
+                    case 1: // CADASTRO DO EXÉRCITO
+                        printf("--- Cadastro de Novo Exrcito ---\n\n");
+
+                        if (totalterritorio < NUM_TERRITORIO) {
+                            printf("Digite o nome do exercito: ");
+                            fgets(biblioteca[totalterritorio].NOME, TAM_STRING, stdin);
+
+                            printf("Digite a cor do exercito: ");
+                            fgets(biblioteca[totalterritorio].COR, TAM_STRING, stdin);
+
+                            printf("Digite o mapa onde o exercito será inserido: ");
+                            fgets(biblioteca[totalterritorio].MAPA, TAM_STRING, stdin);
+
+                            biblioteca[totalterritorio].NOME[strcspn(biblioteca[totalterritorio].NOME, "\n")] = 0; // Remove o caractere de nova linha
+                            biblioteca[totalterritorio].COR[strcspn(biblioteca[totalterritorio].COR, "\n")] = 0;
+                            biblioteca[totalterritorio].MAPA[strcspn(biblioteca[totalterritorio].MAPA, "\n")] = 0;
+
+                            printf("Digite o numero de tropas: ");
+                            scanf("%d", &biblioteca[totalterritorio].TROPAS);
+                            limpaBufferEntrada(); //Limpa algum comando inválido deixado pelo scanf.
+
+                            totalterritorio++;
+                            printf("Exército cadastrado com sucesso!\n");     
+                        } else {
+                            printf("Biblioteca cheia! Não é possível cadastrar mais Exércitos.\n");
+                        }
+                        break;
+
+                    case 2: // LISTAGEM DE LIVROS
+                        printf("--- Lista de Exércitos Cadastrados ---\n\n");
+
+                        if (totalterritorio == 0) {
+                            printf("Nenhum exército cadastrado.\n");
+                        } else {
+                            for (int i = 0; i < totalterritorio; i++) {
+                                printf("-----------------------------\n");
+                                printf("Exército %d:\n", i + 1);
+                                printf("Nome: %s\n", biblioteca[i].NOME);
+                                printf("Cor: %s\n", biblioteca[i].COR);
+                                printf("Tropas: %d\n", biblioteca[i].TROPAS);
+                                printf("Mapa: %s\n", biblioteca[i].MAPA);
+                            }
+                             printf("-----------------------------\n");
+                        }
+
+                        // A pausa é crucial para que o usuário veja a lista antes
+                        // do próximo loo limpar a tela.
+                        printf("\nPressione Enter para continuar...");
+                        getchar();
+                        break;    
+                    
+                    case 0: // SAIR
+                        printf("\nSaindo do sistema...");
+                        break;
+
+                    default: //OPÇÃO INVÁLIDA
+                        printf("Opção inválida! Tente novamente.\n");
+                        printf("\nPressione Enter para continuar...");
+                        getchar();
+                        break;
+            }
+
+        } while (opcao != 0);
+
+//////////////////////////////////////////////////
+
+
+
+//printf("ola mundo!\n");
 return 0;
-}
+}   
